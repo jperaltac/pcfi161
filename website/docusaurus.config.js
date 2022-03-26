c9c6accd4ed44b588a5a418adaf85053cc87ad07
Joaquin Peralta <jperaltac@gmail.com>
2022-03-26 20:51:59 -0300

    Changes in config to put weeks folder

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -23,6 +23,12 @@ module.exports = {
           label: 'Docs',
           position: 'left',
         },
+	{
+	  to: 'weeks/',
+	  activeBasePath: 'weeks',
+	  label: 'Weeks',
+	  position: 'left',
+	},
         {to: 'blog', label: 'Blog', position: 'left'},
         {
           href: 'https://gitlab.com/pages/docusaurus',
@@ -87,7 +93,14 @@ module.exports = {
           editUrl:
             'https://github.com/facebook/docusaurus/edit/master/website/',
         },
-        blog: {
+	weeks:
+        {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: 
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
