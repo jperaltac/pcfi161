422181c6658bea853b64157d51226ada6ccc675e
Joaquin Peralta <jperaltac@gmail.com>
2022-07-29 23:38:25 -0400

    Add setvarinside.sh

---
+++ lectures/setvarinside.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+cd $1
+git rm logo-python.png  
+git rm logo-tux.png  
+git rm logo-unab.png  
+git rm preamble.tex
+ln -v ../logo-python.png logo-python.png
+ln -v ../logo-tux.png logo-tux.png
+ln -v ../logo-unab.png logo-unab.png
+ln -v ../preamble.tex preamble.tex
+git add .
+git commit -m "update preamble"
+git push
+cd ../
+
